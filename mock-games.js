d23d896801d01f092f0055546855598b2c2b186d
Braden Miller <btmiller264@gmail.com>
2021-03-15 15:35:04 -0600

    Add Mock Data

---
+++ mock-games.js
@@ -0,0 +1,163 @@
+let mock = [{
+    id: 1,
+    date: "24 February, 2021",
+    home: "Borussia Monchengladbach",
+    away: "Manchester City",
+    homeScore: 0,
+    awayScore: 2,
+    leg: 1,
+    message: "2nd leg to take place on 16 March, 2021."
+  },
+  {
+    id: 2,
+    date: "16 March, 2021",
+    home: "Manchester City",
+    away: "Borussia Monchengladbach",
+    homeScore: "-",
+    awayScore: "-",
+    leg: 2,
+    message: "2nd leg to take place on 16 March, 2021."
+  },
+  {
+    id: 3,
+    date: "23 February, 2021",
+    home: "SS Lazio",
+    away: "Bayern Munich",
+    homeScore: 1,
+    awayScore: 4,
+    leg: 1,
+    message: "2nd leg to take place on 17 March, 2021."
+  },
+  {
+    id: 4,
+    date: "17 March, 2021",
+    home: "Bayern Munich",
+    away: "SS Lazio",
+    homeScore: "-",
+    awayScore: "-",
+    leg: 2,
+    message: "2nd leg to take place on 17 March, 2021."
+  },
+  {
+    id: 5,
+    date: "23 February, 2021",
+    home: "Atletico Madrid",
+    away: "Chelsea",
+    homeScore: 0,
+    awayScore: 1,
+    leg: 1,
+    message: "2nd leg to take place on 17 March, 2021."
+  },
+  {
+    id: 6,
+    date: "17 March, 2021",
+    home: "Chelsea",
+    away: "Atletico Madrid",
+    homeScore: "-",
+    awayScore: "-",
+    leg: 2,
+    message: "2nd leg to take place on 17 March, 2021."
+  },
+  {
+    id: 7,
+    date: "16 February, 2021",
+    home: "RB Leipzig",
+    away: "Liverpool",
+    homeScore: 0,
+    awayScore: 2,
+    leg: 1,
+    message: "Liverpool win 4-0 on aggregate and advance to quarter finals."
+  },
+  {
+    id: 8,
+    date: "10 March, 2021",
+    home: "Liverpool",
+    away: "RB Leipzig",
+    homeScore: 2,
+    awayScore: 0,
+    leg: 2,
+    message: "Liverpool win 4-0 on aggregate and advance to quarter finals."
+  },
+  {
+    id: 9,
+    date: "17 February, 2021",
+    home: "FC Porto",
+    away: "Juventus",
+    homeScore: 2,
+    awayScore: 1,
+    leg: 1,
+    message: "FC Porto win 4-4 on aggregate (Porto win on away goals) and advance to quarter finals."
+  },
+  {
+    id: 10,
+    date: "9 March, 2021",
+    home: "Juventus",
+    away: "FC Porto",
+    homeScore: 3,
+    awayScore: 2,
+    leg: 2,
+    message: "FC Porto win 4-4 on aggregate (Porto win on away goals) and advance to quarter finals."
+  },
+  {
+    id: 11,
+    date: "16 February, 2021",
+    home: "FC Barcelona",
+    away: "Paris Saint-Germain",
+    homeScore: 1,
+    awayScore: 4,
+    leg: 1,
+    message: "Paris Saint-Germain win 5-2 on aggregate and advance to quarter finals."
+  },
+  {
+    id: 12,
+    date: "10 March, 2021",
+    home: "Paris Saint-Germain",
+    away: "FC Barcelona",
+    homeScore: 1,
+    awayScore: 1,
+    leg: 2,
+    message: "Paris Saint-Germain win 5-2 on aggregate and advance to quarter finals."
+  },
+  {
+    id: 13,
+    date: "17 February, 2021",
+    home: "Sevilla",
+    away: "Borussia Dortmund",
+    homeScore: 2,
+    awayScore: 3,
+    leg: 1,
+    message: "Borussia Dortmund win 5-4 on aggregate and advance to quarter finals."
+  },
+  {
+    id: 14,
+    date: "9 March, 2021",
+    home: "Borussia Dortmund",
+    away: "Sevilla",
+    homeScore: 2,
+    awayScore: 2,
+    leg: 2,
+    message: "Borussia Dortmund win 5-4 on aggregate and advance to quarter finals."
+  },
+  {
+    id: 15,
+    date: "24 February, 2021",
+    home: "Atalanta",
+    away: "Real Madrid",
+    homeScore: 0,
+    awayScore: 1,
+    leg: 1,
+    message: "2nd leg to take place on 16 March, 2021."
+  },
+  {
+    id: 16,
+    date: "16 March, 2021",
+    home: "Real Madrid",
+    away: "Atalanta",
+    homeScore: "-",
+    awayScore: "-",
+    leg: 2,
+    message: "2nd leg to take place on 16 March, 2021."
+  },
+]
+
+export default mock;
